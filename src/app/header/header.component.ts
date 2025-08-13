5c01b224d475909e4306e52cebbcbb58c299cda0
Nikunj Mochi <mochinikunj2@gmail.com>
2025-08-13 10:58:06 +0530

    Adding logic for light/dark theme setup

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+enum ThemeType {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  ngOnInit(): void {
+    const storedTheme = localStorage.getItem('theme') as ThemeType;
+    this.applyTheme(storedTheme || this.detectSystemTheme());
+  }
 
+  private detectSystemTheme(): ThemeType {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? ThemeType.DARK
+      : ThemeType.LIGHT;
+  }
+
+  private applyTheme(theme: ThemeType) {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }
+
+  toggleTheme() {
+    const current = document.documentElement.getAttribute(
+      'data-theme',
+    ) as ThemeType;
+
+    this.applyTheme(
+      current === ThemeType.DARK ? ThemeType.LIGHT : ThemeType.DARK,
+    );
+  }
 }
