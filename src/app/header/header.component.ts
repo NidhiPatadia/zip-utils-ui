eda8b597de1d117db40ac3ce9e071e0e03c05493
Nikunj Mochi <mochinikunj2@gmail.com>
2025-08-14 21:38:20 +0530

    Solving browser apis undefined error caused due to SSR

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 enum ThemeType {
   LIGHT = 'light',
@@ -13,23 +14,42 @@ enum ThemeType {
   styleUrl: './header.component.css',
 })
 export class HeaderComponent implements OnInit {
+  private readonly isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
   ngOnInit(): void {
-    const storedTheme = localStorage.getItem('theme') as ThemeType;
-    this.applyTheme(storedTheme || this.detectSystemTheme());
+    let theme = ThemeType.LIGHT;
+
+    // if code is executing in browser then get theme from localStorage or user prefrence
+    if (this.isBrowser) {
+      const storedTheme = localStorage.getItem('theme') as ThemeType;
+      theme = storedTheme || this.detectSystemTheme();
+    }
+
+    this.applyTheme(theme);
   }
 
   private detectSystemTheme(): ThemeType {
+    if (!this.isBrowser) return ThemeType.LIGHT;
+
     return window.matchMedia('(prefers-color-scheme: dark)').matches
       ? ThemeType.DARK
       : ThemeType.LIGHT;
   }
 
   private applyTheme(theme: ThemeType) {
+    if (!this.isBrowser) return;
+
     document.documentElement.setAttribute('data-theme', theme);
     localStorage.setItem('theme', theme);
   }
 
   toggleTheme() {
+    if (!this.isBrowser) return;
+
     const current = document.documentElement.getAttribute(
       'data-theme',
     ) as ThemeType;
