92f8c6c545e8eb76655721895d200d7edeaeb33c
Nikunj Mochi <mochinikunj2@gmail.com>
2025-10-11 11:47:32 +0530

    Updating BE url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  nodeUrl: 'https://api.ziputils.com/graphql',
+  nodeUrl:
+    'https://clxt716nb4.execute-api.ap-south-1.amazonaws.com/prod/graphql',
   angularUrl: 'https://ziputils.com',
 };
