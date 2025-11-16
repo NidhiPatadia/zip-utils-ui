76c17d5d5ab7df940bf85c3e653b1ea0e9014f95
Nikunj Mochi <mochinikunj2@gmail.com>
2025-11-16 11:41:42 +0530

    Adding redirect logic

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,9 @@
+import { MAIN_DOMAIN } from '../app/enums/common';
+
 export const environment = {
   production: false,
-  nodeUrl: 'https://dev-api.ziputils.com/graphql',
-  angularUrl: 'https://dev.ziputils.com',
+  nodeUrl: `https://dev-api.${MAIN_DOMAIN}/graphql`,
+  angularUrl: `https://dev.${MAIN_DOMAIN}`,
+  textViewerUrl: `https://dev-t.${MAIN_DOMAIN}`,
+  urlViewerUrl: `https://dev-u.${MAIN_DOMAIN}`,
 };
