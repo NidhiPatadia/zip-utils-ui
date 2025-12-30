8fcf3734d80de1beabbc70b263ab59a6c8905cfa
Nidhi Patadia <nidhi.patadia@merkle.com>
2025-12-30 20:56:19 +0530

    Added dynamic open graph.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { ZipTextComponent } from './zip-text/zip-text.component';
 import { ZipTextViewerComponent } from './zip-text/text-viewer/text-viewer.component';
 import { ZipUrlComponent } from './zip-url/zip-url.component';
 import { ZipQrComponent } from './zip-qr/zip-qr.component';
+import { SocialMetaResolver } from './resolvers/social-meta.resolver';
+import { PAGE_TITLE, PAGE_DESCRIPTION } from './enums/common';
 
 export const routes: Routes = [
   {
@@ -14,14 +16,32 @@ export const routes: Routes = [
   {
     path: 'text',
     component: ZipTextComponent,
+    resolve: { social: SocialMetaResolver },
+    data: {
+      pageTitle: PAGE_TITLE.ZIP_TEXT,
+      pageDescription: PAGE_DESCRIPTION.ZIP_TEXT,
+      canonical: '/text',
+    },
   },
   {
     path: 'url',
     component: ZipUrlComponent,
+    resolve: { social: SocialMetaResolver },
+    data: {
+      pageTitle: PAGE_TITLE.ZIP_URL,
+      pageDescription: PAGE_DESCRIPTION.ZIP_URL,
+      canonical: '/url',
+    },
   },
   {
     path: 'qr',
     component: ZipQrComponent,
+    resolve: { social: SocialMetaResolver },
+    data: {
+      pageTitle: PAGE_TITLE.ZIP_QR,
+      pageDescription: PAGE_DESCRIPTION.ZIP_QR,
+      canonical: '/qr',
+    },
   },
   {
     path: 't/:id',
